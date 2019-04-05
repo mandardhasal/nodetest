7b3645ea8bfb9239cecbc8385a4cab5fe23668c6
Mandar Dhasal <mandar.dhasal@gmail.com>
2019-04-05 09:22:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var http = require('http');
+var os = require("os");
+var hostname = os.hostname();
 //loads http module
 var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //sets the right header and status code
-  res.end('Hello World this is test file k44\n');
+  res.end('Hello World -- host-'+hostname);
   //outputs string with line end symbol
-}).listen(1337, "127.0.0.1");
+}).listen(80, "0.0.0.0");
 //sets port and IP address of the server
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running on port 80');
 
